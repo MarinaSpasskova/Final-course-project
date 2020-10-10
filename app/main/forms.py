2d5d7de8a29eb997a05eff4b4b6ccc38fc01b66e
MarinaSpasskova <nerra@yandex.ru>
2020-10-10 21:39:34 +0300

    Final course project part II

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,30 +5,6 @@ from app.models import User
 from wtforms import StringField, TextAreaField,SubmitField
 from wtforms.validators import DataRequired, Length
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember me')
-    submit = SubmitField('Sing In')
-
-
-class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    password_2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
-
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user is not None:
-            raise ValidationError('Please, use a different username')
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is not None:
-            raise ValidationError('Please, use a different email address')
-
 
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -51,4 +27,7 @@ class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
-
+class PostForm(FlaskForm):
+    post = TextAreaField('Write something...', validators=[
+        DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Submit')
