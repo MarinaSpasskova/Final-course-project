63f9a66214275826a5e8732e7d2d13b368a56417
MarinaSpasskova <nerra@yandex.ru>
2020-11-01 15:26:50 +0300

    Extra changes in backend api

--- backend/app/api/tokens.py
+++ backend/app/api/tokens.py
@@ -4,12 +4,14 @@ from app.api import bp
 from app.api.auth import basic_auth, token_auth
 
 
+@bp.route('/auth/login', methods=['POST'])
 @bp.route('/tokens', methods=['POST'])
 @basic_auth.login_required
 def get_token():
     token = g.current_user.get_token()
     db.session.commit()
-    return {'token': token}
+    return {'token': token,
+            'userId': g.current_user.id}
 
 
 @bp.route('/tokens', methods=['DELETE'])
